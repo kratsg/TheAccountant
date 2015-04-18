c3df42d931a93f8630fe4104a3e9ff6d1733778e
Giordon Stark <kratsg@gmail.com>
2015-04-18 14:26:49 -0500

    Clean up LinkDef after using the make_skeleton to auto-generate the new Preselect class for #6.

--- Root/LinkDef.h
+++ Root/LinkDef.h
@@ -1,20 +1,12 @@
 #include <TheAccountant/Preselect.h>
-
 #include <TheAccountant/Audit.h>
 
 #ifdef __CINT__
-
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ nestedclass;
 
-#endif
-
-#ifdef __CINT__
 #pragma link C++ class Audit+;
-#endif
-
-#ifdef __CINT__
 #pragma link C++ class Preselect+;
 #endif
