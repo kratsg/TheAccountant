9ca6adee4925df85b32131ca4385e2ad84eddf8c
Giordon Stark <kratsg@gmail.com>
2015-04-21 17:57:03 -0500

    Fix the scale to be in GeV

--- Root/JetMETHists.cxx
+++ Root/JetMETHists.cxx
@@ -19,7 +19,7 @@ EL::StatusCode TheAccountant::JetMETHists::execute( const xAOD::JetContainer* je
   // sum of leading N jets in event
   for(int i=0; i < std::min<int>( m_numLeadingJets, jets->size() ); ++i) sum_jetPt += jets->at(i)->pt();
 
-  m_effectiveMass->Fill( sum_jetPt + met->met(), eventWeight );
+  m_effectiveMass->Fill( (sum_jetPt + met->met())/1.e3, eventWeight );
 
   return EL::StatusCode::SUCCESS;
 }
