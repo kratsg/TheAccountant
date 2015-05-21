01a86adb8d7674f22504475849aaab2cd903534b
Giordon Stark <kratsg@gmail.com>
2015-05-21 11:31:02 -0500

    VariableDefinitions for optimization

--- TheAccountant/VariableDefinitions.h
+++ TheAccountant/VariableDefinitions.h
@@ -2,19 +2,23 @@
 #define TheAccountant_VariableDefinitions_H
 
 // EDM includes
-#include "xAODEventInfo/EventInfo.h"
 #include "xAODJet/JetContainer.h"
 #include "xAODMuon/MuonContainer.h"
 #include "xAODEgamma/ElectronContainer.h"
-#include "xAODEgamma/PhotonContainer.h"
-#include "xAODTau/TauJetContainer.h"
-#include "xAODTruth/TruthParticleContainer.h"
 #include "xAODMissingET/MissingETContainer.h"
-//#include "xAODBTaggingEfficiency/BTaggingEfficiencyTool.h"
-#include "xAODBTagging/BTagging.h"
 
+/* Caveats: input containers are assumed sorted */
 namespace VariableDefinitions {
+  // Effective Mass calculated using exclusive or inclusive definition depending on args
+  //    Exclusive: Meff(met, jets, 4, 0, 0)
+  //    Inclusive: Meff(met, jets, jets->size(), muons, els)
+  float Meff(const xAOD::MissingET* met, const xAOD::JetContainer* jets, int numJets, const xAOD::MuonContainer* muons, const xAOD::ElectronContainer* els);
 
+  // HT calculated using all jets, will include muons and/or electrons if specified
+  float HT(const xAOD::JetContainer* jets, const xAOD::MuonContainer* muons, const xAOD::ElectronContainer* els);
+
+  // Transverse Mass calculated using MET and leading lepton
+  float mT(const xAOD::MissingET* met, const xAOD::MuonContainer* muons, const xAOD::ElectronContainer* els);
 };
 
 #endif
