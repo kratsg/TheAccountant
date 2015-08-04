0fa03fc287b6a84ee62f1fb22989fd4ec6c92269
Giordon Stark <kratsg@gmail.com>
2015-08-04 10:10:35 -0500

    dPhiMin to 0.0 as default

--- scripts/CookTheBooks.py
+++ scripts/CookTheBooks.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
   group_preselect.add_argument('--jet_minPhi',  type=float, metavar='', help='small-R jet min phi',  default = -4.0)
   group_preselect.add_argument('--jet_maxPhi',  type=float, metavar='', help='small-R jet max phi',  default = 4.0)
   group_preselect.add_argument('--bTag_wp',     type=str,   metavar='', help='small-R jet b-tag working point',  default = "Loose")
-  group_preselect.add_argument('--dPhiMin',     type=float, metavar='', help='dPhiMin(jet, met) cut to apply.', default=0.4)
+  group_preselect.add_argument('--dPhiMin',     type=float, metavar='', help='dPhiMin(jet, met) cut to apply.', default=0.0)
   group_preselect.add_argument('--minMET',      type=float, metavar='', help='Missing Et cut [GeV]', default=0.0)
   group_preselect.add_argument('--baselineLeptonSelection',  type=str,   metavar='', help='Pass in string of the format XX#. XX can be {"==", "!=", ">=", "<=", " >", " <"} and # is a number from 0-9. Note that for " >" and " <" - you need the preceding space.', default="")
   group_preselect.add_argument('--signalLeptonSelection',  type=str,   metavar='', help='Pass in string of the format XX#. XX can be {"==", "!=", ">=", "<=", " >", " <"} and # is a number from 0-9. Note that for " >" and " <" - you need the preceding space.', default="")
