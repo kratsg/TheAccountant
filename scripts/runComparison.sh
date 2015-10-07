d1df14b5300edfb9e0adba2bc81c646cfe176531
Giordon Stark <kratsg@gmail.com>
2015-10-06 19:41:00 -0500

    bump minmet to 200 to make the ntuples a bit smaller, help with cutflow too

--- scripts/runComparison.sh
+++ scripts/runComparison.sh
@@ -6,10 +6,10 @@ ttbarExcFiles="user.lvalery:user.lvalery.407012.ttbar.DAOD_SUSY10.*_tag_10_v1_ou
 
 globalOpts=(-v -f -yyyyyy --optimizationDump --inputDQ2 --badJetVeto --triggerSelection="HLT_xe70")
 
-run0L_a=(--baselineLeptonSelection="==0" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=50 --dPhiMin=0.4 --jet_maxEta=2.8 --jet_minEta=-2.8)
-run0L_b=(--signalLeptonSelection="==0" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=50 --dPhiMin=0.4 --jet_maxEta=2.8 --jet_minEta=-2.8)
-run1L=(--signalLeptonSelection="==1" --jet_minNum=2 --jet_minPt=30 --bjet_minNum=1 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=50 --dPhiMin=0.0 --jet_maxEta=2.8 --jet_minEta=-2.8)
-outputDir="TA07_MBJ10V5"
+run0L_a=(--baselineLeptonSelection="==0" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.4 --jet_maxEta=2.8 --jet_minEta=-2.8)
+run0L_b=(--signalLeptonSelection="==0" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.4 --jet_maxEta=2.8 --jet_minEta=-2.8)
+run1L=(--signalLeptonSelection="==1" --jet_minNum=2 --jet_minPt=30 --bjet_minNum=1 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.0 --jet_maxEta=2.8 --jet_minEta=-2.8)
+outputDir="TA07_MBJ10V6"
 
 mkdir -p "${outputDir}"
 
