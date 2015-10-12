41184f1b68a4435add12559bf885d1dc5af3a7cd
Giordon Stark <kratsg@gmail.com>
2015-10-12 15:44:53 -0500

    MBJ13V5 files

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-dataFiles="user.lvalery.*.data.*_tag_05_output_xAOD.root/"
-gttFiles="user.lvalery.*.Gtt.*_tag_05_output_xAOD.root/"
-dijetFiles="user.mleblanc.*.dijet.*_tag_06_output_xAOD.root/"
-topEWFiles="user.lgagnon.*.topEW.*_tag_05_01_output_xAOD.root/"
-dibosonFiles="user.lgagnon.*.diboson.*_tag_05_output_xAOD.root/"
-singletopFiles="user.lvalery.*.singletop.*_tag_05_01_output_xAOD.root/"
-WsherpaFiles="user.gstark.*.W_sherpa*.*_tag_05_output_xAOD.root/"
-ZsherpaFiles="user.lgagnon.*.Z_sherpa.*_tag_05_output_xAOD.root/"
-ttbarIncFiles="user.amarzin.410000.ttbar.DAOD_SUSY10.e3698_s2608_s2183_r6765_r6282_p2375_tag_06_v3_output_xAOD.root/"
-ttbarExcFiles="user.amarzin.407012.ttbar.DAOD_SUSY10.e4023_s2608_r6765_r6282_p2375_tag_06_v3_output_xAOD.root/"
+dataFiles="user.mswiatlo.*.data.*_tag_13_xAOD_5_output_xAOD.root/"
+gttFiles="user.mswiatlo.*.Gtt.*_tag_13_xAOD_5_output_xAOD.root/"
+dijetFiles="user.mswiatlo.*.dijet.*_tag_13_xAOD_5_output_xAOD.root/"
+topEWFiles="user.mswiatlo.*.topEW.*_tag_13_xAOD_5_output_xAOD.root/"
+#dibosonFiles="user.mswiatlo.*.diboson.*_tag_13_xAOD_5_output_xAOD.root/"
+singletopFiles="user.mswiatlo.*.singletop.*_tag_13_xAOD_5_output_xAOD.root/"
+WsherpaFiles="user.mswiatlo.*.W_sherpa.*_tag_13_xAOD_5_output_xAOD.root/"
+ZsherpaFiles="user.mswiatlo.*.Z_sherpa.*_tag_13_xAOD_5_output_xAOD.root/"
+ttbarIncFiles="user.mswiatlo.410000.ttbar.*_tag_13_xAOD_5_output_xAOD.root/"
+ttbarExcFiles="user.mswiatlo.407012.ttbar.*_tag_13_xAOD_5_output_xAOD.root/"
 
 globalOpts=(-v -f -yyyyyy --optimizationDump --inputDQ2 --badJetVeto --electrons="FinalSignalElectrons" --muons="FinalSignalMuons" --jets="FinalSignalJets")
 
-run0L=(--baselineLeptonSelection="==0" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=50 --dPhiMin=0.4)
-run1L=(--signalLeptonSelection=" >0" --jet_minNum=2 --jet_minPt=30 --bjet_minNum=1 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=50 --dPhiMin=0.4)
-outputDir="TA06_MBJ05"
+run0L=(--signalLeptonSelection="==0" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.4 --jet_maxEta=2.8 --jet_minEta=-2.8)
+run1L=(--signalLeptonSelection="==1" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.0 --jet_maxEta=2.8 --jet_minEta=-2.8)
+outputDir="TA01_MBJ13V5"
 
 mkdir -p "${outputDir}"
 
@@ -24,7 +24,7 @@ CookTheBooks.py --files="${dataFiles}" "${globalOpts[@]}" "${run0L[@]}" --submit
 CookTheBooks.py --files="${gttFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/Gtt_0L" condor > "${outputDir}/Gtt_0L.log" 2>&1 &
 CookTheBooks.py --files="${dijetFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/dijet_0L" condor > "${outputDir}/dijet_0L.log" 2>&1 &
 CookTheBooks.py --files="${topEWFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/topEW_0L" condor > "${outputDir}/topEW_0L.log" 2>&1 &
-CookTheBooks.py --files="${dibosonFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/diboson_0L" condor > "${outputDir}/diboson_0L.log" 2>&1 &
+#CookTheBooks.py --files="${dibosonFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/diboson_0L" condor > "${outputDir}/diboson_0L.log" 2>&1 &
 CookTheBooks.py --files="${singletopFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/singletop_0L" condor > "${outputDir}/singletop_0L.log" 2>&1 &
 CookTheBooks.py --files="${WsherpaFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/Wsherpa_0L" condor > "${outputDir}/Wsherpa_0L.log" 2>&1 &
 CookTheBooks.py --files="${ZsherpaFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/Zsherpa_0L" condor > "${outputDir}/Zsherpa_0L.log" 2>&1 &
@@ -36,7 +36,7 @@ CookTheBooks.py --files="${dataFiles}" "${globalOpts[@]}" "${run1L[@]}" --submit
 CookTheBooks.py --files="${gttFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/Gtt_1L" condor > "${outputDir}/Gtt_1L.log" 2>&1 &
 CookTheBooks.py --files="${dijetFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/dijet_1L" condor > "${outputDir}/dijet_1L.log" 2>&1 &
 CookTheBooks.py --files="${topEWFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/topEW_1L" condor > "${outputDir}/topEW_1L.log" 2>&1 &
-CookTheBooks.py --files="${dibosonFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/diboson_1L" condor > "${outputDir}/diboson_1L.log" 2>&1 &
+#CookTheBooks.py --files="${dibosonFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/diboson_1L" condor > "${outputDir}/diboson_1L.log" 2>&1 &
 CookTheBooks.py --files="${singletopFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/singletop_1L" condor > "${outputDir}/singletop_1L.log" 2>&1 &
 CookTheBooks.py --files="${WsherpaFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/Wsherpa_1L" condor > "${outputDir}/Wsherpa_1L.log" 2>&1 &
 CookTheBooks.py --files="${ZsherpaFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/Zsherpa_1L" condor > "${outputDir}/Zsherpa_1L.log" 2>&1 &
