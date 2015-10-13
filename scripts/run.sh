db7ccba5f8c0b32eb487bf358bae98d60169111b
Giordon Stark <kratsg@gmail.com>
2015-10-13 16:06:29 -0500

    update the run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,7 +11,7 @@ ZsherpaFiles="user.mswiatlo.*.Z_sherpa.*_tag_13_xAOD_5_output_xAOD.root/"
 ttbarIncFiles="user.mswiatlo.410000.ttbar.*_tag_13_xAOD_5_output_xAOD.root/"
 ttbarExcFiles="user.mswiatlo.407012.ttbar.*_tag_13_xAOD_5_output_xAOD.root/"
 
-globalOpts=(-v -f -yyyyyy --optimizationDump --inputDQ2 --badJetVeto --electrons="FinalSignalElectrons" --muons="FinalSignalMuons" --jets="FinalSignalJets")
+globalOpts=(-v -f -yyyyyy --optimizationDump --inputDQ2 --badJetVeto)
 
 run0L=(--signalLeptonSelection="==0" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.4 --jet_maxEta=2.8 --jet_minEta=-2.8)
 run1L=(--signalLeptonSelection="==1" --jet_minNum=4 --jet_minPt=30 --bjet_minNum=2 --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.0 --jet_maxEta=2.8 --jet_minEta=-2.8)
@@ -43,4 +43,4 @@ CookTheBooks.py --files="${ZsherpaFiles}" "${globalOpts[@]}" "${run1L[@]}" --sub
 CookTheBooks.py --files="${ttbarIncFiles}" "${globalOpts[@]}" "${run1L[@]}" --truthMETFilter=" <200" --submitDir="${outputDir}/ttbarInc_1L" condor > "${outputDir}/ttbarInc_1L.log" 2>&1 &
 CookTheBooks.py --files="${ttbarExcFiles}" "${globalOpts[@]}" "${run1L[@]}" --truthMETFilter=" >200" --submitDir="${outputDir}/ttbarExc_1L" condor > "${outputDir}/ttbarExc_1L.log" 2>&1 &
 
-GetWeights.py "${gttFiles}" "${dijetFiles}" "${topEWFiles}" "${dibosonFiles}" "${singletopFiles}" "${WsherpaFiles}" "${ZsherpaFiles}" "${ttbarIncFiles}" "${ttbarExcFiles}" --inputDQ2 -o "${outputDir}/weights.json" > "${outputDir}/weights.log" 2>&1 &
+#GetWeights.py "${gttFiles}" "${dijetFiles}" "${topEWFiles}" "${dibosonFiles}" "${singletopFiles}" "${WsherpaFiles}" "${ZsherpaFiles}" "${ttbarIncFiles}" "${ttbarExcFiles}" --inputDQ2 -o "${outputDir}/weights.json" > "${outputDir}/weights.log" 2>&1 &
