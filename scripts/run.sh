b5f3ac4a1daccbf03046308c40fd3dbc37fb74a1
Giordon Stark <kratsg@gmail.com>
2015-10-27 15:34:34 -0500

    add diboson files

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ dataFiles="user.mswiatlo.*.data.*_tag_13_xAOD_4_output_xAOD.root/"
 gttFiles="user.mswiatlo.*.Gtt.*_tag_13_xAOD_4_output_xAOD.root/"
 dijetFiles="user.mswiatlo.*.dijet.*_tag_13_xAOD_4_output_xAOD.root/"
 topEWFiles="user.mswiatlo.*.topEW.*_tag_13_xAOD_4_output_xAOD.root/"
-#dibosonFiles="user.mswiatlo.*.diboson.*_tag_13_xAOD_4_output_xAOD.root/"
+dibosonFiles="user.mswiatlo.36108*.diboson.*_tag_13_xAOD_6/"
 singletopFiles="user.mswiatlo.*.singletop.*_tag_13_xAOD_4_output_xAOD.root/"
 WsherpaFiles="user.mswiatlo.*.W_sherpa.*_tag_13_xAOD_4_output_xAOD.root/"
 ZsherpaFiles="user.mswiatlo.*.Z_sherpa.*_tag_13_xAOD_4_output_xAOD.root/"
@@ -24,7 +24,7 @@ CookTheBooks.py --files="${dataFiles}" "${globalOpts[@]}" "${run0L[@]}" --submit
 CookTheBooks.py --files="${gttFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/Gtt_0L" condor > "${outputDir}/Gtt_0L.log" 2>&1 &
 CookTheBooks.py --files="${dijetFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/dijet_0L" condor > "${outputDir}/dijet_0L.log" 2>&1 &
 CookTheBooks.py --files="${topEWFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/topEW_0L" condor > "${outputDir}/topEW_0L.log" 2>&1 &
-#CookTheBooks.py --files="${dibosonFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/diboson_0L" condor > "${outputDir}/diboson_0L.log" 2>&1 &
+CookTheBooks.py --files="${dibosonFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/diboson_0L" condor > "${outputDir}/diboson_0L.log" 2>&1 &
 CookTheBooks.py --files="${singletopFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/singletop_0L" condor > "${outputDir}/singletop_0L.log" 2>&1 &
 CookTheBooks.py --files="${WsherpaFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/Wsherpa_0L" condor > "${outputDir}/Wsherpa_0L.log" 2>&1 &
 CookTheBooks.py --files="${ZsherpaFiles}" "${globalOpts[@]}" "${run0L[@]}" --submitDir="${outputDir}/Zsherpa_0L" condor > "${outputDir}/Zsherpa_0L.log" 2>&1 &
@@ -36,7 +36,7 @@ CookTheBooks.py --files="${dataFiles}" "${globalOpts[@]}" "${run1L[@]}" --submit
 CookTheBooks.py --files="${gttFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/Gtt_1L" condor > "${outputDir}/Gtt_1L.log" 2>&1 &
 CookTheBooks.py --files="${dijetFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/dijet_1L" condor > "${outputDir}/dijet_1L.log" 2>&1 &
 CookTheBooks.py --files="${topEWFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/topEW_1L" condor > "${outputDir}/topEW_1L.log" 2>&1 &
-#CookTheBooks.py --files="${dibosonFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/diboson_1L" condor > "${outputDir}/diboson_1L.log" 2>&1 &
+CookTheBooks.py --files="${dibosonFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/diboson_1L" condor > "${outputDir}/diboson_1L.log" 2>&1 &
 CookTheBooks.py --files="${singletopFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/singletop_1L" condor > "${outputDir}/singletop_1L.log" 2>&1 &
 CookTheBooks.py --files="${WsherpaFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/Wsherpa_1L" condor > "${outputDir}/Wsherpa_1L.log" 2>&1 &
 CookTheBooks.py --files="${ZsherpaFiles}" "${globalOpts[@]}" "${run1L[@]}" --submitDir="${outputDir}/Zsherpa_1L" condor > "${outputDir}/Zsherpa_1L.log" 2>&1 &
