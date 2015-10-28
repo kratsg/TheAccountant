38bda114c065e9975ae09d475f638a8311899056
Giordon Stark <kratsg@gmail.com>
2015-10-28 13:10:14 -0400

    update test for SR1 definitions

--- tests/test_cutflow.py
+++ tests/test_cutflow.py
@@ -6,12 +6,12 @@ import sys
 from operator import itemgetter
 
 # the cutflow is defined from top to bottom, adding them in sequence a&&b&&c&&...&&end
-cutflow = [{'name': 'met', 'sel': 'met>=300.', 'count': 100},
-           {'name': 'njet', 'sel': 'multiplicity_jet>=8', 'count': 100},
-           {'name': 'nbjet', 'sel': 'multiplicity_jet_b>=4', 'count': 100},
-           {'name': 'mTb', 'sel': 'mTb>=160.', 'count': 100},
-           {'name': 'meff', 'sel': 'm_effective>=1200.', 'count': 100},
-           {'name': 'ntop', 'sel': 'multiplicity_topTag_veryloose', 'count': 100}]
+cutflow = [{'name': 'preselection', 'sel': '1', 'count': 26270},
+           {'name': 'met', 'sel': 'met>=300.', 'count': 23043},
+           {'name': 'njet', 'sel': 'multiplicity_jet>=8', 'count': 18883},
+           {'name': 'nbjet', 'sel': 'multiplicity_jet_b>=4', 'count': 10159},
+           {'name': 'mTb', 'sel': 'mTb>=160.', 'count': 8257},
+           {'name': 'meff', 'sel': 'm_effective>=1200.', 'count': 8201}]
 
 def main():
   if len(sys.argv) != 2:
@@ -21,16 +21,18 @@ def main():
   t = f.Get('oTree')
 
   for i in range(len(cutflow)):
-    selection = '&&'.join(map(itemgetter('sel'), cutflow[:i+1]))
+    selection = ')&&('.join(map(itemgetter('sel'), cutflow[:i+1]))
+    assString = " {0:>15s} should have {1:6d} raw events."
     if i == 0:
-      assString = "Assert {0:>10s} has {1:6d} raw events."
+      assString = "Assert"+assString
     if i > 0:
-      assString = "  &&   {0:>10s} has {1:6d} raw events."
+      assString = "    &&"+assString
     count = cutflow[i]['count']
     print(assString.format(cutflow[i]['name'], count), end="")
-    actualCount = t.GetEntries(selection)
+    actualCount = t.GetEntries('({0:s})'.format(selection))
     print(" It has {0:6d} raw events.".format(actualCount))
     assert count == actualCount
 
 if __name__ == '__main__':
   main()
+  print("All validated!")
