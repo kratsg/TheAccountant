d308d4223959569fe929fe6fdd8272eb2995382c
Giordon Stark <kratsg@gmail.com>
2016-04-25 14:29:14 -0500

    show example of how we generate weights

---
+++ scripts/example_GetWeights.sh
@@ -0,0 +1,7 @@
+python scripts/GetWeights.py --inputList --inputGrid data/Gtt.list -vvv -y -o Gtt.json
+python scripts/GetWeights.py --inputList --inputGrid data/Wsherpa.list -vvv -y -o Wsherpa.json
+python scripts/GetWeights.py --inputList --inputGrid data/Zsherpa.list -vvv -y -o Zsherpa.json
+python scripts/GetWeights.py --inputList --inputGrid data/diboson.list -vvv -y -o diboson.json
+python scripts/GetWeights.py --inputList --inputGrid data/singletop.list -vvv -y -o singletop.json
+python scripts/GetWeights.py --inputList --inputGrid data/topEW.list -vvv -y -o topEW.json
+python scripts/GetWeights.py --inputList --inputGrid data/ttbar.list -vvv -y -o ttbar.json
