0e5f8aa63ff5b9f99bc0a5403680e21ca2d55f3a
Giordon Stark <kratsg@gmail.com>
2016-06-18 16:44:06 -0500

    add Gbb in the list

--- scripts/example_GetWeights.sh
+++ scripts/example_GetWeights.sh
@@ -1,4 +1,5 @@
 python scripts/GetWeights.py --inputList --inputGrid data/Gtt.list -vvv -y -o Gtt.json
+python scripts/GetWeights.py --inputList --inputGrid data/Gbb.list -vvv -y -o Gtt.json
 python scripts/GetWeights.py --inputList --inputGrid data/Wsherpa.list -vvv -y -o Wsherpa.json
 python scripts/GetWeights.py --inputList --inputGrid data/Zsherpa.list -vvv -y -o Zsherpa.json
 python scripts/GetWeights.py --inputList --inputGrid data/diboson.list -vvv -y -o diboson.json
