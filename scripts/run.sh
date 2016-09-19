b784731c954517ba5c0ecbbe8f3f86a3db2133e7
Giordon Stark <kratsg@gmail.com>
2016-09-19 08:32:47 -0500

    update tag for running

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,7 +15,7 @@ globalOpts=(-v -f -yyyyyy --optimizationDump --inputGrid --inputList --badJetVet
 
 run0L=(--signalLeptonSelection="==0" --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.0)
 run1L=(--signalLeptonSelection="==1" --numLeadingJets=4 --jetLargeR_minPtView=300 --minMET=200 --dPhiMin=0.0)
-outputDir="2016_tag01_v1_dPhiMETMin_0p0"
+outputDir="2.4.18-0-0"
 
 mkdir -p "${outputDir}"
 
